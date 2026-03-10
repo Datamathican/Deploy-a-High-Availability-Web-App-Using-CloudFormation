33513f1d46e867495bbdb41523684715377835d4
Pramod Singh <104872097+Datamathican@users.noreply.github.com>
2026-03-10 20:22:55 +0000

    Add create.sh for AWS CloudFormation stack creation
    Adds a script to create an AWS CloudFormation stack.

---
+++ create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Usage: ./create.sh <stack-name> <template-file> <parameter-file>
+aws cloudformation create-stack \
+--stack-name $1 \
+--template-body file://$2 \
+--parameters file://$3 \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+--region=us-east-1
